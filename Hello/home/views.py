bfd5c16b04bf3418a5a45e22aa1da48cbef349cb
iamluciddreamer <shuklamanasofficial@gmail.com>
2023-10-24 02:59:50 +0530

    Feat : Models Added

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 def index(request):
@@ -15,4 +17,9 @@ def services(request):
     return render(request , "services.html" )
 
 def contact(request):
+    if request.method == 'POST' : 
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        contact = Contact(email = email , password = password, date= datetime.today())
+        contact.save()
     return render(request , "contact.html" )
