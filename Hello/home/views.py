b0440d6ffb1c836cf396dc0073ecc3da648db60e
iamluciddreamer <shuklamanasofficial@gmail.com>
2023-10-24 01:16:41 +0530

    Feat : Template Files

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<p style="color:blue;font-size:46px;">Im a big, blue, <strong>strong</strong> paragraph</p>')
-
+    # return HttpResponse('<p style="color:blue;font-size:46px;">Im a big, blue, <strong>strong</strong> paragraph</p>')
+    context = {
+        'variable1' : "this is sent",
+        'variable2' : "this is sent 2"
+    }
+    return render(request , "index.html",context )
 def about(request):
     return HttpResponse('this is about')
 
