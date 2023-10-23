4f3f2521d6dd5239c31fcba82aa99ca5b73ee8d2
iamluciddreamer <shuklamanasofficial@gmail.com>
2023-10-24 03:13:35 +0530

    Feat : Forms Added

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -22,4 +23,5 @@ def contact(request):
         password = request.POST.get("password")
         contact = Contact(email = email , password = password, date= datetime.today())
         contact.save()
+        messages.success(request , "Your Data has been saved.")
     return render(request , "contact.html" )
