0126a44f23325b58012d57753695b4b055753b98
iamluciddreamer <shuklamanasofficial@gmail.com>
2023-10-24 00:56:40 +0530

    Feat : Static Files

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<p style="color:blue;font-size:46px;">Im a big, blue, <strong>strong</strong> paragraph</p>')
+
+def about(request):
+    return HttpResponse('this is about')
+
+def services(request):
+    return HttpResponse('this is services')
+
+def contact(request):
+    return HttpResponse('this is contact')
